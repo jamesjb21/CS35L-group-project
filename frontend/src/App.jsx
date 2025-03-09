f3bdb0ba2029de9700cec98194553355a5532155
James <jamesjb21@g.ucla.edu>
2025-03-09 01:07:22 -0800

    created routes for starting pages with temp pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import StartPage from './StartPage';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import Login from './pages/login';
+import Register from './pages/register';
+import notFound from './pages/notFound';
+import ProtectedRoute from './components/ProtectedRoute';
 import './index.css'; // Global styles
+import HomePage from './pages/HomePage';
+
+function Logout () {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        {/* Add other routes/webpages here */}
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<notFound />} />
+        <Route path="/homepage" element={<ProtectedRoute> <HomePage /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
