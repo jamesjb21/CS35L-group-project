43f137464ce79b637fcdf2fe181d2c3db4f04ee4
James <jamesjb21@g.ucla.edu>
2025-03-13 20:38:27 -0700

    Revert "Merge main into search-recipe, resolve conflicts"
    This reverts commit aab93393a397ff90c2bafc5d0586071874eb3eb0, reversing
    changes made to de941fe7a70ca24d14dc5c6efd1cccc4cfa5aaa6.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MyUser, Post, Like, Comment
+
+admin.site.register(MyUser)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
 
 # Register your models here.
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -9,8 +9,7 @@ from .serializers import (
     PostSerializer,
     PostCreateSerializer,
     CommentSerializer,
-    LikeSerializer,
-    UserSerializer
+    LikeSerializer
 )
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -19,8 +18,8 @@ import json
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all
-    serializer_class = UserSerializer
+    queryset = MyUser.objects.all
+    serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
 
 @api_view(['GET'])
